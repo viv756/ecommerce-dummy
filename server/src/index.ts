be68ca21ce3c9f4fd33f26fd1a3b269050a0e9ed
*vivek <vrvivekofficial@gmail.com>
2026-01-07 14:46:51 +0530

    feat: add async and error handling middlewares

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,14 @@ import "dotenv/config";
 
 import express from "express";
 import connectDatabase from "./config/database";
+import { errorHandler } from "./middlewares/error-handler";
 
 const PORT = process.env.PORT;
 const app = express();
+
+app.use("")
+
+app.use(errorHandler);
 
 app.listen(PORT, async () => {
   await connectDatabase();
---
+++ server/src/middlewares/error-handler.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || "Server Error",
+  });
+};
