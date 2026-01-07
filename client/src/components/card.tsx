8e3b47d99a4fbf68c1f451c86c2a2a542d1e8004
*vivek <vrvivekofficial@gmail.com>
2026-01-07 16:06:44 +0530

    feat: enhance product display by integrating API fetching and updating Card component

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -1,19 +1,23 @@
 import { BsFillCartPlusFill } from "react-icons/bs";
 
-const Card = () => {
+type Props = {
+  product: {
+    name: string;
+    price: number;
+    image: string;
+  };
+};
+
+const Card = ({ product }: Props) => {
   return (
     <div className="block max-w-[18rem] rounded-lg shadow-lg">
-      <div className="relative  bg-cover bg-no-repeat">
-        <img
-          className="rounded-t-lg"
-          src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-          alt=""
-        />
+      <div className="relative  bg-cover bg-no-repeat w-full">
+        <img className="rounded-t-lg object-cover w-full h-50" src={`${product.image}`} alt="img" />
       </div>
       <div className="p-6 flex flex-col gap-4">
-        <p className="text-base font-semibold">Product Name</p>
+        <p className="text-base font-semibold">{product.name}</p>
         <div className="flex justify-between items-center">
-          <p>$180</p>
+          <p>{product.price}</p>
           <BsFillCartPlusFill size={25} />
         </div>
       </div>
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,51 @@
+import { useEffect, useState } from "react";
 import Card from "../components/card";
 import Navbar from "../components/navbar";
+import { baseURL } from "../lib/baseUrl";
 
 const HomePage = () => {
-  return <div className="">
-    <Navbar />
-    <Card/>
-  </div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+
+        const res = await fetch(`${baseURL}/product/get/all`);
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.message || "Failed to fetch products");
+        }
+
+        setProducts(data.products); // adjust based on API response
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="p-10">
+        {loading && <p>Loading products...</p>}
+        {error && <p className="text-red-500">{error}</p>}
+
+        <div className="grid sm:grid-cols-4 gap-6 place-content-center">
+          {products.map((product) => (
+            <Card key={product._id} product={product} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
